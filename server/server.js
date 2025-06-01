132d8593816e57ee04acce360d3028e0d82f074a
Shweta Sanjay Ingole <115386998+shwetasingole@users.noreply.github.com>
2025-06-01 12:33:16 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -18,8 +18,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173", // Replace with your frontend URL
-    credentials: true, // Allow credentials (cookies, headers)
+    origin: "http://localhost:5173" 
   })
 );
 dbConnect();
